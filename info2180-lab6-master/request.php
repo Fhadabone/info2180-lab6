decef1803a0bf8b644b8bed6cd91e5856ce4b7ee
Leroy <leroy.palmer@mymona.uwi.edu>
2018-11-13 22:18:37 +0000

    Exercise1

--- info2180-lab6-master/request.php
+++ info2180-lab6-master/request.php
@@ -1,7 +1,7 @@
 <?php
-// accept a term (keyword)
+ // accept a term (keyword)
 // respond with a value
-$query = $_GET['q'];
+ $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -11,5 +11,5 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>"; 
+ print "<h3>" . strtoupper($query) . "</h3>";
+print "<p>" . $definition[$query] . "</p>";
